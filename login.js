e3e6bda2e0d998ae6c672fa921eada7f20c31577
ryu-k67 <200441067@ccmailg.meijo-u.ac.jp>
2022-02-17 22:33:32 +0900

    ログイン・ユーザ追加更新

--- login.js
+++ login.js
@@ -1,108 +1,81 @@
-  let db=firebase.firestore();
-  let userid="";
+let db=firebase.firestore();
+let userid="";
+var userName="";
 
-  //DOM取得
-  var inputarea = document.getElementById('input-area');
-  var newuser = document.getElementById('newuser');
-  var login = document.getElementById('login');
-  var mypage = document.getElementById('mypage');
-//  var logout = document.getElementById('logout');
-  var info = document.getElementById('info');
-  
-  
-  
-  //新規登録処理
-  newuser.addEventListener('click', function(e) {
-    var name= document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    .catch(function(error) {
-      alert('登録できません（' + error.message + '）');
-    });
 
+//新規登録処理
+function createUser() {
+  userName= document.getElementById('name').value;
+  var mailAddress = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  
+  firebase.auth().createUserWithEmailAndPassword(mailAddress, password)
+  .then(function(){  
     userid = firebase.auth().currentUser.uid;
-
-    db.collection(userid).add({
-      name:name,
-      password:password
+    db.collection("account").document(userid).set({
+      name:userName,
+      mailadd:mailAddress,
+      pass:password
+    })
+    .then(function(){
+      console.log("登録成功!");
     })
     .catch(function (error) { // 失敗した場合に実行される箇所
       console.error("Error adding document: ", error);
     });
+  })
+  .catch(function(error) {
+    alert('登録できません（' + error.message + '）');
   });
-  
-  
-  
-  //ログイン処理
-  login.addEventListener('click', function(e) {
-    var name= document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    
-    firebase.auth().signInWithEmailAndPassword(email, password)
-    .catch(function(error) {
-      alert('ログインできません（' + error.message + '）');
-    });
+}
 
+//ログイン処理
+function login() {
+  var userName= document.getElementById('name').value;
+  var mailAddress = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  
+  firebase.auth().signInWithEmailAndPassword(mailAddress, password)
+  .then(function(){
     userid = firebase.auth().currentUser.uid;
-
-    db.collection(userid).add({
-      name:name,
-      password:password
-    })
-    .catch(function (error) { // 失敗した場合に実行される箇所
-      console.error("Error adding document: ", error);
-    });
+    loginDisplay();
+  })
+  .catch(function(error) {
+    alert('ログインできません（' + error.message + '）');
   });
-  
-  
-  
-  //ログアウト処理
+}
+
+//ログアウト処理
 /*
-  logout.addEventListener('click', function() {
-    firebase.auth().signOut();
-  });
+logout.addEventListener('click', function() {
+  firebase.auth().signOut();
+});
 */  
 
-mypage.addEventListener('click', function(e) {
-  window.location="login.html";
+//認証状態の確認
+firebase.auth().onAuthStateChanged(function(user) {
+  if(user) {
+    userid = firebase.auth().currentUser.uid;
+    loginDisplay();
+  }
+  else {
+  //  logoutDisplay();
+  }
 });
-  
-  
-  //認証状態の確認
-  firebase.auth().onAuthStateChanged(function(user) {
-    if(user) {
-      userid = firebase.auth().currentUser.uid;
 
-      loginDisplay();
-    }
-    else {
-    //  logoutDisplay();
-    }
-  });
-  
-  
-  
-  function loginDisplay() {
-  //  logout.classList.remove('hide');
-    inputarea.classList.add('hide');
-    getUserName();
-    info.textContent = "ログイン中です！";
-    window.location="mypage.html";
-  }
-  
+function loginDisplay() {
+//  logout.classList.remove('hide');
+  inputarea.classList.add('hide');
+  info.textContent = "ログイン中です!";
+  window.location="mypage.html";
+}
+
 /*  
-  function logoutDisplay() {
-    logout.classList.add('hide');
-    inputarea.classList.remove('hide');
-  
-    info.textContent = "";
-  }
+function logoutDisplay() {
+  logout.classList.add('hide');
+  inputarea.classList.remove('hide');
+
+  info.textContent = "";
+}
 */
 
-function getUserName(){
-  var userName=db.collection(userid).get().data();
-  return userName;
-}
