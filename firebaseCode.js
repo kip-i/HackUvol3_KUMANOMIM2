d5f36a2d3ef52261119e233a69f85428080169fa
takanofumi <k.takafmi0413@gmail.com>
2022-02-18 11:53:12 +0900

    createprojectを修正しました

--- firebaseCode.js
+++ firebaseCode.js
@@ -8,17 +8,14 @@ let db = firebase.firestore(); // データベースに関する機能の取得
 *projectStartPeriod     :Date
 *projectEndPeriod       :Date
 */
-function createProject( projectName, projectStartPeriod, projectEndPeriod,projectUrl){
+function createProject( projectName, projectStartPeriod, projectEndPeriod,projectId){
 
     /*日時をyyyymmdd(y:年,m:月,d:日)の形に変換*/
     var startTime = (projectStartPeriod.getFullYear * 10000) + (projectStartPeriod.getMonth * 100) + (projectStartPeriod.getDate);
     var endTime = (projectEndPeriod.getFullYear * 10000) + (projectEndPeriod.getMonth * 100) + (projectEndPeriod.getDate);
     
-    //URLの保存
-    var url = nill;
-
-    //データベースにドキュメントを追加.決まっていいない値はnullか0
-    db.collection("project").add({
+    //データベースにドキュメントを更新.決まっていいない値はnullか0
+    db.collection("project").doc(projectId).set({
         URL: url,
         menberId:  [""],
         projectName: projectName,
@@ -33,7 +30,7 @@ function setMySchedule( uid, mySchedule, date){
     
     var scheduleDate =  (date.getFullYear * 10000) + (date.getMonth * 100) + (date.getDate);
     
-    db.collection("account").document(uid).collection("mySchedule").add({
+    db.collection("account").doc(uid).collection("mySchedule").add({
         date: scheduleDate,
         mySchedule: mySchedule
     })
@@ -42,14 +39,37 @@ function setMySchedule( uid, mySchedule, date){
 /*my日程を取得する関数*/
 function getMySchedule(date, uid){
 
-    var mySchedule = [];
 
-    db.collection("account").document(uid).collection("myScheduleId").where("date","==",date).get()
+    var today = new Date();     //今日の日付を取得
+
+    var intToday = (today.getFullYear * 10000) + (today.getMonth * 100) + (today.getDate);      //今日の日付をintの形に変換
+
+    var pastDataId = [];    //過去の日付のドキュメントIDを入れる配列
+    var i = 0;              //ループ用
+
+    //過去の日付のデータ検索
+    db.collection("account").doc(uid).collection("myScheduleId").where("date","<",intToday).get()
     .then(
         (querySnapshot) => {
             querySnapshot.forEach((doc) => {
                 var data = doc.data();
-                mySchedule.push([doc.id,data.mySchedule,data.date]);
+                pastDataId[i] = data.id;
+                i++;
+        });
+    }).catch((error) => {
+        console.log("データ取得失敗(${error})");
+    });
+
+    var mySchedule = [[]];
+   
+
+    db.collection("account").doc(uid).collection("myScheduleId").where("date","==",date).get()
+    .then(
+        (querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                var data = doc.data();
+                mySchedule[i].push([doc.id,data.mySchedule,data.date]);
+                i++;
         });
     }).catch((error) => {
         console.log("データ取得失敗(${error})");
