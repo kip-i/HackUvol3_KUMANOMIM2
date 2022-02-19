b6853b23858558e1e2bb1f1a9bbc792e810b60a8
ryu-k67 <200441067@ccmailg.meijo-u.ac.jp>
2022-02-20 04:55:47 +0900

    プロジェクト表の表示まで

--- firebaseCode.js
+++ firebaseCode.js
@@ -1,6 +1,8 @@
 
 
 /*firebaseの関数を書いたファイル*/
+let db = firebase.firestore;
+
 
 function getParam(name, url) {
     if (!url) url = window.location.href;
@@ -11,6 +13,7 @@ function getParam(name, url) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
+
 //Date型の日付をintの形に変換
 function transDateToInt(date){
     
@@ -39,9 +42,9 @@ function createProject( projectName, projectStartPeriod, projectEndPeriod,projec
         URL: url,
         menberId:  [null],
         projectName: projectName,
-        projectPeriod: [startTime,endTime],
+        projectPeiriod: [startTime,endTime],
         projectDecisionName: 0,
-        projectMemberName: [null]
+        projectmenberName: [null]
     })
 }
 
@@ -61,24 +64,24 @@ function setMySchedule( uid, mySchedule){
                 scheduleId[i] = doc.id;
                 i++;
             });
-        }).catch((error) => {
+
+            var index = 0;  //配列は全部つながっているので分ける時に使用するインデックス
+            //ループして全部更新
+            for(i = 0;i < 60;i++,index = index + 144){
+                scheduleDate = transDateToInt(today);   //日付の指定
+                
+                //データの保存
+                db.collection("account").doc(uid).collection("mySchedule").doc(scheduleId[i]).set({
+                    date: scheduleDate,
+                    mySchedule: mySchedule.slice(index,index + 144)
+                })
+                
+                today.setDate(today.getDate() + 1);     //日付を1日進める
+            }
+        }
+        ).catch((error) => {
         console.log("データ取得失敗(${error})");
     });
-    
-    var index = 0;  //配列は全部つながっているので分ける時に使用するインデックス
-
-    //ループして全部更新
-    for(i = 0;i < 60;i++,index = index + 144){
-        scheduleDate = transDateToInt(today);   //日付の指定
-
-        //データの保存
-        db.collection("account").doc(uid).collection("mySchedule").doc(scheduleId[i]).set({
-            date: scheduleDate,
-            mySchedule: mySchedule.slice(index,index + 144)
-        })
-
-        today.setDate(today.getDate() + 1);     //日付を1日進める
-    }
 }
 
 /*my日程を取得する関数.小塚*/
@@ -86,60 +89,65 @@ function getMySchedule(uid){
 
     var today = new Date();     //今日の日付を取得
     var finalDay = new Date();
-
+    
     var stringDay;
-
-    finalDay.setDate(finalDay.getDate + 59);    //登録可能な一番遠い日付を入れておく
-
+    
+    finalDay.setDate(finalDay.getDate() + 59);    //登録可能な一番遠い日付を入れておく
+    
     stringDay = today.getFullYear() + "-" + today.getMonth() + "-" + today.getDate();
-
+    
     var intToday = transDateToInt(stringDay);      //今日の日付をintの形に変換
-
+    
     var pastDataId = [];     //過去の日付のドキュメントIDを入れる配列
     var defaultPeriod = [];  //初期状態の日程
     var i = 0;               //ループ用
-
+    
     //初期状態の生成
     for(i = 0;i < 144;i++){
         defaultPeriod[i] = 1;
     }
-
+    
     i = 0;  //iの初期化
+    var idBuff = [];
     //過去の日付のデータ検索.ドキュメントIDを保存
     db.collection("account").doc(uid).collection("myScheduleId").where("date","<",intToday).get()
     .then(
         (querySnapshot) => {
             querySnapshot.forEach((doc) => {
                 //var data = doc.data();
+                console.log(doc.id);
                 pastDataId[i] = doc.id;
                 i++;
         });
+        
+        //過去のデータを更新
+        for(i = 0;i < pastDataId.length;i++){
+          console.log("データ更新開始");
+          stringDay = finalDay.getFullYear() + "-" + finalDay.getMonth() + "-" + finalDay.getDate();
+          var intDate = transDateToInt(stringDay);
+          //過去のデータのドキュメントを入力されていない日付のデータとして更新
+          db.collection("account").doc(uid).collection("myScheduleId").doc(pastDataId[i]).set({
+            date : intDate,
+            mySchedule : defaultPeriod
+          })
+          finalDay.setDate(finalDay.getDate() - 1);
+      }
     }).catch((error) => {
         console.log("データ取得失敗(${error})");
     });
-
-    //過去のデータを更新
-    for(i = 0;i < pastDataId.length;i++){
-
-        //過去のデータのドキュメントを入力されていない日付のデータとして更新
-        db.collection("account").doc(uid).collection("myScheduleId").doc(pastDataId[i]).set({
-            date : transDateToInt(finalDay),
-            mySchedule : defaultPeriod
-        })
-
-        finalDay.setDate(finalDay.getDate - 1);
-    }
-
+    
     var mySchedule = [];      //今日から順にデータを入れておく
     var data; 
-
+    
+    console.log("データ取得開始");
     //日付で昇順にして日程を取得
     db.collection("account").doc(uid).collection("myScheduleId").orderBy("date").get()
     .then(
         (querySnapshot) => {
             querySnapshot.forEach((doc) => {
-                data = doc.data();
-
+                data = doc.data()["mySchedule"];
+    
+                console.log("でーた取得中");
                 //値渡しで日程をコピー
                 for(var k = 0; k < 144;k++){
                     mySchedule[mySchedule.length + k] = data[k];
@@ -148,62 +156,66 @@ function getMySchedule(uid){
     }).catch((error) => {
         console.log("データ取得失敗(${error})");
     });
-
+    
     return mySchedule;
 }
 
 /*firebaseの関数を書いたファイル*/ 
 //let db = firebase.firestore(); // データベースに関する機能の取得
 
-async function getProjectMembers(){
-    let ID = getParam("project");
+function getProjectMembers(){
+    let ID;
     var buff;
-    db.collection("project").doc(ID).get()
-    .then((query)=>{
-        buff = query.data()["projectmenberName"];
+
+    ID = getParam("project");
+    buff = db.collection("project").doc(ID).data()["projectMemberName"];
+    /*.then((query)=>{
+        var buff = [];
+        query.forEach((doc)=>{
+            var data = doc.data();
+            buff.push([data.projectMemberName]);
+        });
     })
     .catch((error)=>{
         console.log("データの取得に失敗しました(${error})");
     });
-    console.log(buff);
+    */
     return buff;//参加者の名前が入った配列を返す
 }
-async function getProjectPeriodStart(){
-    let ID = getParam("project");
-    var buff;
-    var date;
-    date=await db.collection("project").doc(ID).get()
-    .then((doc)=>{
-        buff = doc.data()["projectPeiriod"];
-        let temp=parseInt(buff[0]);
-        console.log(temp);
-        return temp;   
-    })
-    console.log(date);
-    var start=new Date(date/10000,(date%10000)/100,(date%100));
-    console.log(start);
+
+function getProjectperiodStart(){
+    let ID;
+    ID = getParam("project");
+    //start = new Date();
+    var docRef=db.collction("project").doc(ID);
+    //docRef.get().then((doc)=>{
+        var buff = doc.data()["projectPeriod[0]"];
+        //buff.push([doc.projectPeriod[0]]);
+    //})
+
+    let start=new Date(buff[0]/10000,(buff[0]%10000)/100,(buff[0]%100));
     return start;
 }
 
-async function getProjectPeriodFinish(){
-    let ID = getParam("project");
-    var buff;
-    var date;
-    date=await db.collection("project").doc(ID).get()
-    .then((doc)=>{
-        buff = doc.data()["projectPeiriod"];
-        let temp=parseInt(buff[1]);   
-        return temp;
-    })
-    let end=new Date(date/10000,(date%10000)/100,(date%100));
-    console.log(end);
+function getProjectPeriodFinish(){
+    let ID;
+    
+    ID = getParam("project");
+    //end = new date();
+
+    var docRef = db.collection("project").doc(ID);
+    //docRef.get().then((doc)=>{
+        var buff = doc.data()["projectPeriod[1]"];
+    //    buff.push([doc.projectPeriod[1]]);
+    //})
+    let end=new Date(buff[1]/10000,(buff[1]%10000)/100,(buff[1]%100));
     return end;
 }
 
 function getProjectMemberSchedule(menberIndex){
     var data = [];
 
-    db.collection("project").doc(ID).collection(projectMenberPeriod).where("menberIndex","==",menberIndex)
+    db.collection("project").doc(ID).collection(projectMenberPeriod).where("memberIndex","==",memberIndex)
     .get()
     .then((querySnapshot) =>{
         querySnapshot.forEach((doc) => {
@@ -217,4 +229,12 @@ function getProjectMemberSchedule(menberIndex){
 }
 
 
-
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
